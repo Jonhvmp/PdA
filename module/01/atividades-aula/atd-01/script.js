e68b2692fc4c35a9fd13c6d3ab5ebfc1c54bc21a
Jonhvmp <Jonhpaz08@gmail.com>
2024-08-21 17:29:14 -0300

    Criando um sistema de quantos dias faltam para acabar a semana

--- module/01/atividades-aula/atd-01/script.js
+++ module/01/atividades-aula/atd-01/script.js
@@ -1,9 +1,22 @@
 // Declarando as variáveis
-var notaJoao = "9"; // tipo string
-var notaMaria = 10; // tipo number
+var notaJoao = "9"; // tipo string, pois está entre aspas
+var notaMaria = 10; // tipo number, pois não está entre aspas
 
 // Convertendo a string para numero e somando as variáveis
-var media = parseInt(notaJoao) + notaMaria;
+var media = parseInt(notaJoao) + notaMaria; // 9 + 10 = 19, 'perseInt' converte a string para número
 
 // Exibindo o resultado
 console.log("A soma das notas é: " + media); // A soma das notas é: 19
+
+// Calculando os dias restantes para terminar a semana
+var diasSemana = 7 - new Date().getDay(); // 7 - 3 = 4, onde 3 é o dia da semana atual (quarta-feira).
+
+// Exibindo o resultado dos dias restantes para terminar a semana
+console.log("Faltam " + diasSemana + " dias para terminar a semana.");
+
+
+// versão 2
+var diasSemana2 = 7 - 3; // 7 - 3 = 4, onde 3 é o dia da semana atual (quarta-feira).
+
+// Exibindo o resultado
+console.log("Faltam " + diasSemana2 + " dias para terminar a semana.");
