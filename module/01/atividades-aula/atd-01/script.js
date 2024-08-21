3d895aafdf5388d3c029eeaab5bc8f12e19ee00b
Jonhvmp <Jonhpaz08@gmail.com>
2024-08-21 17:08:47 -0300

    Estilizar margens das sections na página inicial

---
+++ module/01/atividades-aula/atd-01/script.js
@@ -0,0 +1,9 @@
+// Declarando as variáveis
+var notaJoao = "9"; // tipo string
+var notaMaria = 10; // tipo number
+
+// Convertendo a string para numero e somando as variáveis
+var media = parseInt(notaJoao) + notaMaria;
+
+// Exibindo o resultado
+console.log("A soma das notas é: " + media); // A soma das notas é: 19
