9e6e795bf4abc88349cd1771b52b82ca35584ce2
Jonhvmp <Jonhpaz08@gmail.com>
2024-08-26 16:48:16 -0300

    condicionais js

---
+++ module/01/atividades-aula/condicionais/script.js
@@ -0,0 +1,100 @@
+// camelCase 
+var primeiroNome = 'Jonh';
+
+// snake_case
+var segundo_nome = 'Alex';
+
+// quais tipos de dados podemos ter?
+// string = 'texto'
+// number = 1, 2, 3
+// boolean = true, false
+// null = null
+// undefined = undefined
+// object = {}
+// symbol = Symbol()
+
+// exemplos de dados
+var nome = 'Jonh';
+var idade = 30;
+var solteiro = true;
+var carro = null;
+var moto; // undefined
+
+var pessoa = {
+    nome: 'Jonh',
+    idade: 30,
+    solteiro: true,
+    carro: null,
+    moto: undefined
+};
+
+// como acessar os dados de um objeto?
+console.log(pessoa.nome);
+console.log(pessoa['nome']);
+
+// como alterar os dados de um objeto?
+pessoa.nome = 'Alex';
+pessoa['nome'] = 'Alex';
+
+// Quais são as operações que podemos executar?
+// + = adição
+// - = subtração
+// * = multiplicação
+// / = divisão
+// % = módulo
+// ++ = incremento
+// -- = decremento
+// = = atribuição
+// == = igualdade
+// === = igualdade de valor e tipo
+// != = diferente
+// !== = diferente de valor e tipo
+// > = maior que
+// < = menor que
+// >= = maior ou igual
+// <= = menor ou igual
+// && = e
+// || = ou
+// ! = negação
+
+// Estruturas de controle
+// if = se
+// else = senão
+// else if = senão se
+// switch = escolha
+// while = enquanto
+// do while = faça enquanto
+// for = para
+// for in = para dentro
+// for of = para de
+// break = parar
+// continue = continuar
+// return = retornar
+// try = tentar
+// catch = pegar
+// finally = finalmente
+
+// portugol
+// se (condição) {
+//     comando a ser executado caso a condição seja VERADEIRA
+// } senão {
+//     comando a ser executado caso a condição seja FALSA
+// }
+
+// Estruturas de repetição
+// while (condição) {
+//     comando a ser executado enquanto a condição for VERADEIRA
+// }
+
+// Estruturas de dados
+// array = []
+// object = {}
+// map = new Map()
+// set = new Set()
+// weakMap = new WeakMap()
+// weakSet = new WeakSet()
+// promise = new Promise()
+// queue = new Queue()
+// stack = new Stack()
+// linkedList = new LinkedList()
+// tree = new Tree()
