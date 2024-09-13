7403aa84def043b57bd78403982cfcb2b8ae5961
Jonhvmp <Jonhpaz08@gmail.com>
2024-09-13 09:45:09 -0300

    Refatorar função de soma para exibir resultado com mais clareza

--- module/01/atividades-aula/functions04/script.js
+++ module/01/atividades-aula/functions04/script.js
@@ -4,16 +4,16 @@ function adicionarValores(valorA, valorB) {
 
   // Função que mostra o resultado
   function mostrarResultado() {
-    alert("O resultado da soma é: " + resultadoFinal);
+    alert("O resultado da soma dos números é: " + resultadoFinal);
   }
 
   // Chama a função
   mostrarResultado();
 }
 
-// Pede ao usuário que insira dois valores
+// prompt aq
 let primeiroValor = parseFloat(prompt("Insira o primeiro valor:"));
 let segundoValor = parseFloat(prompt("Insira o segundo valor:"));
 
-// Chama a função de soma
+// Chamando a função de soma
 adicionarValores(primeiroValor, segundoValor);
